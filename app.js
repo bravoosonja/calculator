39a2518a1fd435b1cef53811da5e82082978d299
bravoosonja <bravoosonja@gmail.com>
2022-04-01 09:33:18 +1100

    creates necessary files, complete basic functions

---
+++ app.js
@@ -0,0 +1,34 @@
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+//takes an operator and 2 numbers and then calls one of the above functions on the numbers
+function operate(operator, a, b) {
+  a = Number(a);
+  b = Number(b);
+  switch (operator) {
+    case "+":
+      return add(a, b);
+    case "-":
+      return subtract(a, b);
+    case "x":
+      return multiply(a, b);
+    case "÷":
+      if (b === 0) return null;
+      else return divide(a, b);
+    default:
+      return null;
+  }
+}
