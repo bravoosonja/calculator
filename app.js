e62ba5cad1f57638a036da3731b2f4df0123a71c
bravoosonja <bravoosonja@gmail.com>
2022-04-01 12:41:27 +1100

    update UI

--- app.js
+++ app.js
@@ -1,3 +1,70 @@
+//buttons & selectors
+const numberButtons = document.querySelectorAll("[data-number]");
+const operatorButtons = document.querySelectorAll("[data-operator]");
+const equalsButton = document.querySelector("[data-equals]");
+const deleteButton = document.querySelector("[data-delete]");
+const clearButton = document.querySelector("[data-clear]");
+const pointButton = document.getElementById("point-btn");
+const lastOperationScreen = document.getElementById("last-operation-screen");
+const currentOperationScreen = document.getElementById(
+  "current-operation-screen"
+);
+
+//displays - class calculator
+//?? is this necessary?
+class Calculator {
+  constructor(lastOperationScreen, currentOperationScreen) {
+    this.lastOperationScreen = lastOperationScreen;
+    this.currentOperationScreen = currentOperationScreen;
+    this.clear();
+  }
+}
+
+const calculator = new Calculator(lastOperationScreen, currentOperationScreen);
+
+//event listeners
+window.addEventListener("keydown", handleKeyboardInput);
+equalsButton.addEventListener("click", evaluate);
+clearButton.addEventListener("click", clear);
+deleteButton.addEventListener("click", deleteNumber);
+pointButton.addEventListener("click", appendPoint);
+
+numberButtons.forEach((button) =>
+  button.addEventListener("click", () =>
+    calculator.appendNumber(button.textContent)
+  )
+);
+
+operatorButtons.forEach((button) =>
+  button.addEventListener("click", () =>
+    calculator.updateDisplay(button.textContent)
+  )
+);
+
+//deletes all values and clears screen
+function clear() {
+  this.currentOperationScreen = "";
+  this.lastOperationScreen = "";
+  this.operation = undefined;
+}
+
+//Deletes everything; equivalent to AC
+function deleteOne() {}
+
+//takes user input
+function appendNumber() {}
+
+//controls operation
+//? is this necessary?
+function chooseOperation() {}
+
+//displays result
+function compute() {}
+
+//updates values inside of screen
+function updateDisplay() {}
+
+//arithmetic functions
 function add(a, b) {
   return a + b;
 }
